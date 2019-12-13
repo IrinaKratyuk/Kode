ddfe375e3c6e61b1517ea62cb83f4d1359780876
IrinaKratyuk <irischk17@gmail.com>
2019-12-13 16:56:25 +0300

    zakritie

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -125,6 +125,7 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
